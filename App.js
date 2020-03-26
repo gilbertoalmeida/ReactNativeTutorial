2b3e378ca0ae5754d3590c57d787258f793a12ee
Gilberto Almeida <almeidanetogilberto@gmail.com>
2020-03-26 23:38:12 +0100

    Setup of the project.

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{color: 'darkslateblue', fontSize: 30}}>Hey ho</Text>
+    </View>
+  );
+};
+
+export default App;
